ca363b1c3add36c21949f04325d55bc1ee803aee
brandao2334 <149542030+brandao2334@users.noreply.github.com>
2024-11-15 03:51:55 -0300

    Delete src directory homolog

--- src/Backend/BackHydra/models/Report.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const reportSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  consumption: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('Report', reportSchema);
